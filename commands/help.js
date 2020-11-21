aff4c8c1d11f8956d79c97b87255921459361fd2
Lojjo18 <loollalojjo-1998@hotmail.com>
2020-11-22 02:26:00 +0300

    First

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {//رأس إنشاء ملف موديول
     description: "A guide for using the bot.",//وصف الأمر
     execute(message, args){//دالة التنفيذ للأمر، مع الرسالة والمدخل كمدخلات للدالة
         info = "This bot encrypts your messages in an efficient way!";
-        warn = "*WARNING!* It's highly recomended to write the commands on a private room or through DM of the bot.";
+        warn = "*WARNING!* It's highly recomended to write the commands on a private room or through DM of the bot, also, you're responsible for remebering your keys after encryption.";
         lis = "List of commands (using 'ciph' as a prefix):";
         encrypt = "encrypt: Encrypting the input text and sending the cipher text through DM (requires keys, 10 characters max).";
         decrypt = "decrypt: Decrypting the input cipher text and sending the cipher text through DM (requires keys, 10 characters max).";
